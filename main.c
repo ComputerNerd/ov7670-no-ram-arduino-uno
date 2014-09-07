d636056f8f6980743e042ee530fc85476d52d4ee
ComputerNerd <mailboxer123@yahoo.com>
2014-09-07 15:56:50 -0500

    Correct README

--- main.c
+++ main.c
@@ -115,7 +115,7 @@ int main(void){
 	TWBR=72;//set to 100khz
 	//enable serial
 	UBRR0H=0;
-	UBRR0L=1;//3 = 0.5M 2M baud rate = 0 7 = 250k 207 is 9600 baud rate
+	UBRR0L=1;//0 = 2M baud rate. 1 = 1M baud. 3 = 0.5M. 7 = 250k 207 is 9600 baud rate.
 	UCSR0A|=2;//double speed aysnc
 	UCSR0B = (1<<RXEN0)|(1<<TXEN0);//Enable receiver and transmitter
 	UCSR0C=6;//async 1 stop bit 8bit char no parity bits
